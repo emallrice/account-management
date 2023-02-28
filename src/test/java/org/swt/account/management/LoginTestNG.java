fcda04253ab981733690bcdbcb373090b83e2a3c
maotou-spy <phucbuihuu2604@gmail.com>
2023-02-28 14:46:32 +0700

    add LoginTestNG using TestNG Test Case

---
+++ src/test/java/org/swt/account/management/LoginTestNG.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/EmptyTestNGTest.java to edit this template
+ */
+package org.swt.account.management;
+
+import static org.testng.Assert.*;
+
+/**
+ *
+ * @author Yemufu
+ */
+public class LoginTestNG {
+    
+    public LoginTestNG() {
+    }
+    
+}
